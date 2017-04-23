98244b6ca407f56d2b053ed4f5d6b6a92e22c6c9
Jirayut Keawchuen <jirayut_deknoi@hotmail.co.th>
2017-04-23 10:41:58 +0700

    web config

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ import Menu from './menu';
 class App extends Component{
     
     render(){
+var http = require('http');
 
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Azure!");
+
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
         return(
             <Router>
                 <Switch>
